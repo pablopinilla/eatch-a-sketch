47e52512afc421a06286cec87b64af9ec0147ff2
Pablo Pinilla <140816109+pablopinilla@users.noreply.github.com>
2023-08-14 14:38:07 -0500

    Color picker, random and shades of grey functionality added

--- app.js
+++ app.js
@@ -4,21 +4,61 @@ const gridWraper = document.querySelector(".gridWraper")
 const gridContainer = document.querySelector(".gridContainer")
 for (let i = 0; i < 256; i++){
     let newDiv = document.createElement("div")
-    newDiv.textContent = "HO"
     newDiv.classList.add("square")
     gridContainer.appendChild(newDiv)
 }
+const picker = document.querySelector(".picker")
+let color = picker.value
+picker.addEventListener("input", ()=>{
+    color = picker.value
+})
 
-//Hover effect
-function hover() {
-const squares = document.querySelectorAll(".square")
-squares.forEach(square => {
+//Paint effect
+function paint () {
+    const squares = document.querySelectorAll(".square")
+    squares.forEach(square => {
     square.addEventListener("mouseover", ()=>{
-        square.classList.add("hovered")
+        square.style.backgroundColor = `${color}`
     })
 });
 }
-hover()
+
+//Erase effect 
+function erase() {
+    const squares = document.querySelectorAll(".square")
+    squares.forEach(square => {
+    square.addEventListener("mouseover", ()=>{
+        square.style.backgroundColor = "white"
+    })
+});
+}
+
+//Random effet
+function rainbow() {
+    const squares = document.querySelectorAll(".square")
+    squares.forEach(square => {
+    square.addEventListener("mouseover", ()=>{
+        square.style.backgroundColor = `${randomColor()}`
+    })
+});
+}
+
+//Darkening effect
+function darken() {
+    const squares = document.querySelectorAll(".square")
+    let i = 50
+    squares.forEach(square => {
+    square.addEventListener("mouseover", ()=>{
+        i++
+        square.style.backgroundColor = `rgb(${255 - i},${255 - i},${255 - i})`
+    })
+});    
+}
+
+//Random Color Generator
+function randomColor() {
+    return "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase();
+}
 
 //Button prompt
 let numOfSquares = 0
@@ -29,8 +69,8 @@ numOfSquares = numOfSquar ** 2
 createGrid(numOfSquares)
 })
 
-//Create grid function
 
+//Create grid function
 function createGrid(numOfSquares){
     const newGridContainer = document.createElement("div");
     newGridContainer.classList.add("gridContainer");
@@ -43,7 +83,4 @@ function createGrid(numOfSquares){
     }
     newGridContainer.style.gridTemplateColumns = `repeat(${(numOfSquar)}, 1fr)`;
     newGridContainer.style.gridTemplateRows = `repeat(${(numOfSquar)}, 1fr)`;
-    hover();
-    //newDiv.style.width = `${1200/numOfSquares}px`
-    //newDiv.style.height = `${1200/numOfSquares}px`
 }
