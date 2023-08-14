f9ee48c0d7a18be9645dc58e60cab1466834ac30
Pablo Pinilla <140816109+pablopinilla@users.noreply.github.com>
2023-08-14 08:49:41 -0500

    Completed the grid functionality system, the reset mechanism and the hoovering effect

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 
 //Initial grid
+const gridWraper = document.querySelector(".gridWraper")
 const gridContainer = document.querySelector(".gridContainer")
 for (let i = 0; i < 256; i++){
     let newDiv = document.createElement("div")
@@ -9,15 +10,40 @@ for (let i = 0; i < 256; i++){
 }
 
 //Hover effect
+function hover() {
 const squares = document.querySelectorAll(".square")
 squares.forEach(square => {
     square.addEventListener("mouseover", ()=>{
         square.classList.add("hovered")
     })
 });
+}
+hover()
 
 //Button prompt
-const newGrid = document.querySelector(".newGrid")
-newGrid.addEventListener("click", ()=>{
-    prompt("Enter a number of squares")
+let numOfSquares = 0
+const newButton = document.querySelector(".newGrid")
+newButton.addEventListener("click", ()=>{
+numOfSquar = prompt("Enter a number of squares")
+numOfSquares = numOfSquar ** 2
+createGrid(numOfSquares)
 })
+
+//Create grid function
+
+function createGrid(numOfSquares){
+    const newGridContainer = document.createElement("div");
+    newGridContainer.classList.add("gridContainer");
+    gridWraper.replaceChildren(newGridContainer);
+
+    for (let i = 0; i < numOfSquares; i++){
+        let newDiv = document.createElement("div");
+        newDiv.classList.add("square");
+        newGridContainer.appendChild(newDiv);
+    }
+    newGridContainer.style.gridTemplateColumns = `repeat(${(numOfSquar)}, 1fr)`;
+    newGridContainer.style.gridTemplateRows = `repeat(${(numOfSquar)}, 1fr)`;
+    hover();
+    //newDiv.style.width = `${1200/numOfSquares}px`
+    //newDiv.style.height = `${1200/numOfSquares}px`
+}
